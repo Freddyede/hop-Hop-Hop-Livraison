6b0987bff34f8aae220c7d1653f85d8b6f6072a5
Franck <patouillardfranck3@gmail.com>
2020-04-28 13:13:35 +0200

    Add progressus template and js file corresponding

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Villes;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('base.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
