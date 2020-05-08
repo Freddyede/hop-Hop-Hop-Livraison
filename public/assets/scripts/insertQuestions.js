4ac1f0197e5574d229210a3b6131b80760ff2fef
Franck <patouillardfranck3@gmail.com>
2020-05-08 21:57:27 +0200

    Finished forms
    ToDos :
     - Récupérer all Valeurs
    - Envoyés toutes les valeurs dans un objets JS

---
+++ public/assets/scripts/insertQuestions.js
@@ -0,0 +1,48 @@
+$(document).on('change','#won',function(){
+    var containerQuestions = $("#next-questions");
+    var containerAllQuestions= $("#select");
+    if($('#won').val() === 'Appartement'){
+        containerAllQuestions.after(`
+            <div class="form-group row" id="otherQuestions">
+                <div class="col-sm-6 col-lg-6 text-center">
+                    <p class="text-orange">
+                        Avez-vous un digicodes :
+                    </p>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <input class="form-check-input" type="radio" name="digicodeGood" id="digicodeGood" value="option1" checked>
+                            <label class="form-check-label" for="exampleRadios1">
+                                Oui
+                            </label>
+                        </div>
+                        <div class="col-sm-6">
+                            <input class="form-check-input" type="radio" name="digicodeGood" id="digicodeGood" value="option1" checked>
+                            <label class="form-check-label" for="exampleRadios1">
+                                Non
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="form-group row" id="otherQuestions2">
+                <div class="col-sm-6 text-center">
+                    <p class="text-orange">
+                        Code digicode :
+                    </p>
+                </div>
+                <div class="col-sm-6 text-right">
+                    <input
+                        id="numberDigicode"
+                        type="number" class="form-control"
+                        name="digicode" min="0"
+                    />
+                </div>
+        </div>
+        `)
+    }else{
+        $("#otherQuestions").remove();
+        $("#otherQuestions2").remove();
+    }
+});
---
+++ public/assets/scripts/sendData.js
@@ -0,0 +1,19 @@
+function sendingData(){
+    var regex = /^(0[1-68])(?:[ _.-]?(\d{2})){4}$/;
+    if(regex.test($("#phone").val()) ){
+        if($("#phone").hasClass('error')){
+            $("#phone").removeClass("error");
+        }
+        $(document).ready(function() {
+            let Habitation = $("#Habitation").val();
+            let phone =  parseInt($("#phone").val());
+            let city = $("#city").val();
+            console.log(Habitation);
+            console.log(phone);
+            console.log(city);
+     
+        });
+    }else{
+        $("#phone").addClass("error");
+    }
+}
